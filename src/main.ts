f1e4bc813e74b8b993e31f8dab1fc92d875211f1
techqria <114258568+techqria@users.noreply.github.com>
2022-12-02 14:57:48 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin: "https://www.vinumeventos.com.br"});
+  app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+      next();
+  });
+
+  app.enableCors({
+      allowedHeaders:"*",
+      origin: "*"
+  });
   await app.listen(process.env.PORT || 3002);
 }
 bootstrap();
