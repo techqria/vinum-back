ff1bd2ab8f3169c86d78130ffd5bbc0c8fea0465
techqria <114258568+techqria@users.noreply.github.com>
2022-12-02 15:05:32 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule,{ cors: true });
+  const options = {
+    "origin": true, 
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 200,
+    "credentials":true,
+    "allowedHeaders": "Content-Type, Accept,Authorization",
+
+  }
+  app.use(cor(options))
   await app.listen(process.env.PORT || 3002);
 }
 bootstrap();
