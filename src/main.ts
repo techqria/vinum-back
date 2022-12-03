9b49940bd6424dd0ff7e6d6e2e60e0fab6ee1fcf
techqria <114258568+techqria@users.noreply.github.com>
2022-12-02 22:13:15 -0300

    putting options in CORS

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(
+  const options = {
     origin: "https://www.vinumeventos.com.br",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  );
-  await app.listen(process.env.PORT || 3002);
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true
+  };
+  app.enableCors(options);
+  await app.listen(3000);
 }
 bootstrap();
