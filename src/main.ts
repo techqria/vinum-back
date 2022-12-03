389febcf9cfcf2f42f70bcf7f8063c83dce37b21
techqria <114258568+techqria@users.noreply.github.com>
2022-12-02 21:48:35 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, { cors: true });
   if(req.method === 'OPTIONS') { return res.status(200).json(({ body: "OK" })) }
   await app.listen(process.env.PORT || 3002);
 }
